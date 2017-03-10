9d08fa2896768652f5b2d8b9c96ef072929cf332
Charles DeLong <c.thecheesedelong@gmail.com>
2017-03-10 13:31:11 -0800

    Got the = to work

--- adventure/main.js
+++ adventure/main.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-  
+  /*
 	$("#add").on("click", function(){
     var val1 = $("#num_one").val()
     var val2 = $("#num_two").val()
@@ -19,5 +19,69 @@ $(document).ready(function(){
     var val1 = $("#num_one").val()
     var val2 = $("#num_two").val()
     $("#answer").val(Number(val1) / Number(val2))
-	})
+	})*/
+
+
+
+
+  $("#clear").on("click", function(){
+    $("#display").html("")
+  })
+
+  
+  $(".number").on("click", function(){
+    var selection = $(this).html();
+    $("#display").append(selection)
+  })
+  $(".opperator").on("click", function(){
+    var selection = $(this).html();
+    $("#display").append(selection)
+  })
+  $("#equals").on("click", function(){
+    var result = eval($("#display").html())
+    $("#display").html(result)
+  })
+  /*
+  $("#two").on("click", function(){
+    $("#display").html("2")
+  })
+  $("#three").on("click", function(){
+    $("#display").html("3")
+  })
+  $("#four").on("click", function(){
+    $("#display").html("4")
+  })
+  $("#five").on("click", function(){
+    $("#display").html("5")
+  })
+  $("#six").on("click", function(){
+    $("#display").html("6")
+  })
+  $("#seven").on("click", function(){
+    $("#display").html("7")
+  })
+  $("#eight").on("click", function(){
+    $("#display").html("8")
+  })
+  $("#nine").on("click", function(){
+    $("#display").html("9")
+  })
+  $("#zero").on("click", function(){
+    $("#display").html("0")
+  })
+  $("#dot").on("click", function(){
+    $("#display").html(".")
+  })
+  */
 })
+
+
+
+
+
+
+
+
+
+
+
