407082f75994b3adea2f89e1e1dd0f9f6eb85951
Charles DeLong <c.thecheesedelong@gmail.com>
2017-03-10 09:11:49 -0800

    All functions on first bit working

--- adventure/main.js
+++ adventure/main.js
@@ -5,4 +5,19 @@ $(document).ready(function(){
     var val2 = $("#num_two").val()
     $("#answer").val(Number(val1) + Number(val2))
 	})
+	$("#subtract").on("click", function(){
+    var val1 = $("#num_one").val()
+    var val2 = $("#num_two").val()
+    $("#answer").val(Number(val1) - Number(val2))
+	})
+	$("#multiply").on("click", function(){
+    var val1 = $("#num_one").val()
+    var val2 = $("#num_two").val()
+    $("#answer").val(Number(val1) * Number(val2))
+	})
+	$("#divide").on("click", function(){
+    var val1 = $("#num_one").val()
+    var val2 = $("#num_two").val()
+    $("#answer").val(Number(val1) / Number(val2))
+	})
 })
